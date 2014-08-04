9c62d08f3a5f9070fc4e5d319f1dd6e3a2028bc7
Frank Chen <frank.chen@gmail.com>
2014-08-03 17:07:03 -0700

    Chapter 3.6
    Rango says hello world

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+	
+	url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
 )
