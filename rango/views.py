c88260560436e7907c12aa28de767dd8b3ffcc36
withfries2 <frank@Franks-MacBook-Pro-3.local>
2014-08-04 07:21:41 -0700

    Chapter 3.7 Exercises
    Added About page

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    return HttpResponse("Rango says <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
